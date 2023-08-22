77970dc91a66bef2c4e3e8865b9defe490e01b51
Abdullahamir221999 <abdullahamir221999@gmail.com>
2023-08-22 17:56:32 +0500

    22nd August Commit

--- Project Testing 3/Assets/!Scripts/LevelSpawnSettings.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "LevelSpawnSettings", menuName = "Custom/Level Spawn Settings")]
-public class LevelSpawnSettings : ScriptableObject
-{
-    public float spawnDuration = 5f;
-    public int totalEnemiesInLevel = 10; // Add this line to store the total enemies in the level.
-}
--- Project Testing 3/Project 2 Testing/Assets/!Scripts/LevelSpawnSettings.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "LevelSpawnSettings", menuName = "Custom/Level Spawn Settings")]
-public class LevelSpawnSettings : ScriptableObject
-{
-    public float spawnDuration = 5f;
-}
